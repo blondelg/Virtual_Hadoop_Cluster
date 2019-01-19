d49fa0b6192710374534e859fa1551c90497be0d
Geoffroy <33812049+blondelg@users.noreply.github.com>
2019-01-19 14:18:29 +0100

    Update tests_on_vagrant.sh

--- tests_on_vagrant.sh
+++ tests_on_vagrant.sh
@@ -1,2 +1,12 @@
+# Do a project direcotory
+mkdir <project_dir>
+
+# Go to the direcory
+cd vagrant_getting_started
+
+# Create a vagrant file to store settings
+vagrant init ubuntu/trusty64
+
 # Load ubuntu server image 
 vagrant box add ubuntu/trusty64
+
